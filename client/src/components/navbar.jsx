b9ad27fc22bb7a6e90c33e24dfc05805bdce3fe4
Ankit pandey <pdyankit01@gmail.com>
2023-09-12 12:22:50 +0530

    final commit

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {ShoppingCart} from "phosphor-react";
+import "./navbar.css";
+function navbar() {
+    return <div className="navbar" >
+        <div className="links" >
+            <Link to='/'>Shop</Link>
+            <Link to="/cart">
+              <ShoppingCart size={32}/>
+            </Link>
+        </div>
+    </div>
+}
+export default navbar
---
+++ client/src/pages/shop/shop.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {PRODUCTS} from "../../products.js"
+import Product from './product'
+import "./shop.css"
+function shop() {
+return <div className="shop" >
+    <div className="shopTitle">
+        <h1>Tech Shop</h1>
+    </div>
+    <div className="products">
+    {PRODUCTS.map((product)=>(
+        <Product
+        data={product}
+        />
+    ))}</div>
+</div>
+}
+export default shop
